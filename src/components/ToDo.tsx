4ef251edd3076f981b5f24d8323621d25e681d1a
Seiwa-Edu <kuma.0131@icloud.com>
2022-02-20 10:44:17 +0900

    [add]大まかな投稿処理作成

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,16 +1,76 @@
-import React from "react";
-import { doc, getDoc } from "firebase/firestore";
+import React, { useState, useEffect } from "react";
+import { db } from "framework/firebase/firebase";
+import {
+  collection,
+  getDocs,
+  addDoc,
+  updateDoc,
+  deleteDoc,
+  doc,
+} from "firebase/firestore";
 
 export const ToDo = () => {
-  const docRef = doc(db, "cities", "SF");
-  const docSnap = await getDoc(docRef);
+  const initialDate = new Date();
+  const [todos, setToDos] = useState([]);
+  const [content, setContent] = useState<string>("");
+  const [userId, setUserId] = useState<string>("");
+  const [due, setDue] = useState<Date>(initialDate);
+
+  const todosCollectionRef = collection(db, "todos");
+
   //投稿機能の実装
-  const handleToDo = () => {};
+  const createToDo = async () => {
+    await addDoc(todosCollectionRef, {
+      content: content,
+      userId: userId,
+      due: due,
+    });
+  };
+  // const updateToDo = () => {
+  //   const userDoc = doc(db, "users", id);
+  //   const newFields = { age: age + 1 };
+  //   await updateDoc(userDoc, newFields);
+  // };
+  // const deleteToDo = () => {
+  //   const userDoc = doc(db, "users", id);
+  //   await deleteDoc(userDoc);
+  // };
+  //投稿一覧取得
+  useEffect(() => {
+    const getToDos = async () => {
+      const data = await getDocs(todosCollectionRef);
+      setToDos(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+    };
+    getToDos();
+  }, []);
 
   return (
     <>
-      <div>ToDo</div>
-      <form onSubmit={handleToDo}></form>
+      <h3>ToDo</h3>
+      <input
+        placeholder="何する。。？"
+        onChange={(e) => {
+          setContent(e.target.value);
+        }}
+      />
+      <div>
+        <label>今日する</label>
+        <input type="radio" name="due" value="今日する" />
+        <label>明日する</label>
+        <input type="radio" name="due" value="明日する" />
+        <label>いつかする</label>
+        <input type="radio" name="due" value="いつかする" />
+      </div>
+      <button onSubmit={createToDo}></button>
+      <h3>リスト</h3>
+      {todos.map((todo) => {
+        return (
+          <div>
+            // 先ずはテキストだけ出す
+            <p>{todo.content}</p>
+          </div>
+        );
+      })}
     </>
   );
 };
--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -1,5 +1,6 @@
 import { useAuthContext } from "framework/context/AuthContext";
 import React from "react";
+import Todo from "components/ToDo";
 
 function UserInfo() {
   const { currentUser } = useAuthContext();
@@ -9,6 +10,7 @@ function UserInfo() {
         <p>名前:{currentUser.displayName}</p>
         <p>メール:{currentUser.email}</p>
       </div>
+      <Todo />
     </>
   );
 }
